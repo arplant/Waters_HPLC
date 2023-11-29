5a38c3755bebdeb2212e20caef978f2d32e284bf
AR Plant <122537357+arplant@users.noreply.github.com>
2023-11-29 11:34:54 +0000

    Update HPLC_Waters_arw.py for efficiency, inverse plotting
    - Added option to plot absorbance data at given timepoint
    - Added options to plot single or multiple figures (exercise caution); refactored loop

--- HPLC_Waters_arw.py
+++ HPLC_Waters_arw.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Process/plot HPLC data from Waters ".arw" files
-
 Created on Thu Apr 14 07:53:55 2022
-@author: A R Plant
+@author: Alast
 
 Things to do:
-    Read files listed by 'files'; decide either to build a 3D dataframe or to make a new frame for each
-    Work out how to parse compound sample names, e.g. dnhA_NEG_1 - split by underscre when reading into dataframe?
-
+    Work out how to parse compound sample names, e.g. dnhA_NEG_1
 """
 
 # Import libraries to select data files (glob), read and process them (pandas) and plot them (matplotlib)
@@ -18,57 +14,120 @@ import matplotlib.pyplot as plt
 
 
 # List all files with .arw extension (i.e. data files) so we can iteratively make dataframes from each file
-# Maybe create dictionary to include data passed from sample name plus associated dataframe
-files = glob.glob('HPLC_data/*.arw')
+files = glob.glob('*.arw')
 for i in files:
     tempDf = pd.read_csv(i)
     print(tempDf)
 
-wave_raw = pd.read_csv("HPLC_data/raw1803.arw", sep = '\t', skiprows=2, header=None, nrows=1)
+#Construct column header list from first file
+wave_raw = pd.read_csv(files[0], sep = '\t', skiprows=2, header=None, nrows=1)
 wave = wave_raw.loc[:,1:]
 wave_head = wave.values.tolist()
 wave_head = wave_head[0]
 wave_head.insert(0,"Time")
 
-HPLC = pd.read_csv("HPLC_data/raw1803.arw", sep = '\t', skiprows=4, header=None)
+#Select timepoint to plot
+#Time intervals:
+#0
+#0.01666667
+#0.03333334
+#0.05
+#0.06666667
+#0.08333334
+#0.1
+timepoint = 8
+maxY = 0.1
+
+
+#Read file, trim header, add header, set row labels, filter by timepoint
+
+#Plot everything in one figure
+for i in files:
+    HPLC = pd.read_csv(i, sep = '\t', skiprows=4, header=None)
+    HPLCfilt = HPLC.loc[:,:]
+    HPLCfilt.columns = wave_head
+    HPLCfilt = HPLCfilt.set_index(['Time'])
+    HPLCrow = HPLCfilt.loc[(timepoint)]
+    plt.title('Abs at T=' + str(timepoint))
+    plt.ylabel('Abs')
+    plt.xlabel('Wavelength')
+    plt.grid(True)
+    plt.ylim((0, maxY))
+    plt.plot(HPLCrow)
+plt.show()
+    
+#Plot separate figures
+for i in files:
+    HPLC = pd.read_csv(i, sep = '\t', skiprows=4, header=None)
+    HPLCfilt = HPLC.loc[:,:]
+    HPLCfilt.columns = wave_head
+    HPLCfilt = HPLCfilt.set_index(['Time'])
+    HPLCrow = HPLCfilt.loc[timepoint]
+    plt.title('Abs for ' + i + 'at T=' + str(timepoint))
+    plt.ylabel('Abs')
+    plt.xlabel('Wavelength')
+    plt.grid(True)
+    plt.ylim((0, maxY))
+    plt.plot(HPLCrow)
+    plt.show()
+
+
+
+
+    
+"""
+#Efficient loop
+wave_raw = pd.read_csv(i, sep = '\t', skiprows=2, header=None, nrows=1)
+wave = wave_raw.loc[:,1:]
+wave_head = wave.values.tolist()
+wave_head = wave_head[0]
+wave_head.insert(0,"Time")
+
+
+for i in files:
+    HPLC = pd.read_csv(i, sep = '\t', skiprows=4, header=None)
+    HPLCfilt = HPLC.loc[:,:]
+    HPLCfilt.columns = wave_head
+    HPLCfilt = HPLCfilt.set_index(['Time'])
+
+    HPLCrow = HPLCfilt.iloc[8]
+    HPLCrow.plot()
+"""
+    
+    
+"""
+#Single run
+wave_raw = pd.read_csv("raw32552.arw", sep = '\t', skiprows=2, header=None, nrows=1)
+wave = wave_raw.loc[:,1:]
+wave_head = wave.values.tolist()
+wave_head = wave_head[0]
+wave_head.insert(0,"Time")
+
+HPLC = pd.read_csv("raw32552.arw", sep = '\t', skiprows=4, header=None)
 HPLCfilt = HPLC.loc[:,:]
 HPLCfilt.columns = wave_head
+HPLCfilt = HPLCfilt.set_index(['Time'])
 
-#HPLCfiltplot = plt.plot(HPLCfilt[300.555])
-HPLCfilt.plot(x = "Time", y = 300.555)
-
-
-# Plot specific wavelength
-plt.figure(figsize = (10,5))
-plt.plot(HPLC['Time'],HPLC['0'], linewidth = 1.5)
-plt.axis([0,40,0,100])
-plt.xlabel('Time (min)')
-plt.ylabel('Abs')
-plt.show()
-
-# Plot sublpots of specific wavelengths
-plt.figure(figsize = (10,5))
-plt.subplot(211)
-plt.plot(HPLC['Time'],HPLC['0'], linewidth = 1.5)
-plt.plot(HPLC['Time'],HPLC['100'], linewidth = 2.0)
-plt.axis([0,15,0,200])
-plt.xlabel('Time (min)')
-plt.ylabel('Abs')
-plt.show()
-
+HPLCrow = HPLCfilt.iloc[0]
+HPLCrow.plot()
+"""    
+    
+    
+    
 """
-# import module
-import pandas as pd
- 
-# assign dataset names
-list_of_names = ['crime','username']
- 
-# create empty list
-dataframes_list = []
- 
-# append datasets into the list
-for i in range(len(list_of_names)):
-    temp_df = pd.read_csv("./csv/"+list_of_names[i]+".csv")
-    dataframes_list.append(temp_df)
- 
- """
+#Inefficient loop
+for i in files:
+    wave_raw = pd.read_csv(i, sep = '\t', skiprows=2, header=None, nrows=1)
+    wave = wave_raw.loc[:,1:]
+    wave_head = wave.values.tolist()
+    wave_head = wave_head[0]
+    wave_head.insert(0,"Time")
+
+    HPLC = pd.read_csv(i, sep = '\t', skiprows=4, header=None)
+    HPLCfilt = HPLC.loc[:,:]
+    HPLCfilt.columns = wave_head
+    HPLCfilt = HPLCfilt.set_index(['Time'])
+
+    HPLCrow = HPLCfilt.iloc[0]
+    HPLCrow.plot()
+"""
